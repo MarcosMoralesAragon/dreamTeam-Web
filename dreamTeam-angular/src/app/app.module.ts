c5bedb49a3a5708a809ba6f991cccc1fc2a0ba14
Marcos Morales Aragon <marcosmoralesaragon@gmail.com>
2022-06-22 23:57:28 +0200

    Angular done

--- dreamTeam-angular/src/app/app.module.ts
+++ dreamTeam-angular/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import {MatIconModule} from '@angular/material/icon'; 
+import {ChartModule} from 'primeng/chart';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule,
+    MatIconModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
